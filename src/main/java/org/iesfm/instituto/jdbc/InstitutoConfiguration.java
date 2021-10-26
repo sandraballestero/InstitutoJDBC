15dcd577235b9595e8fa623e8cf6f28e9de00bb9
mikelsanvi <mikelsanvi@gmail.com>
2021-10-26 09:12:55 +0200

    Added placeholder configuration

--- src/main/java/org/iesfm/instituto/jdbc/InstitutoConfiguration.java
+++ src/main/java/org/iesfm/instituto/jdbc/InstitutoConfiguration.java
@@ -1,13 +1,17 @@
 package org.iesfm.instituto.jdbc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("application.properties")
 public class InstitutoConfiguration {
 
     @Bean
@@ -21,12 +25,18 @@ public class InstitutoConfiguration {
     }
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource(
+            // placeholder
+            @Value("${database.driver}") String driver,
+            @Value("${database.url}") String url,
+            @Value("${database.user}") String user,
+            @Value("${database.password}") String password
+    ) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:13306/Instituto");
-        dataSource.setUsername("root");
-        dataSource.setPassword("federica");
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
         return dataSource;
     }
 }
